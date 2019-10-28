9adef8e247eb00175662025c35801fc521a53c3f
Daniel Braverman <hexular@gmail.com>
2019-10-28 17:32:39 +0000

    created and embedded a header partial into both /views files. Added a render to the /urls request in express_server.

--- express_server.js
+++ express_server.js
@@ -14,8 +14,8 @@ app.get("/", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { url: urlDatabase };
-  res.send(templateVars);
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {

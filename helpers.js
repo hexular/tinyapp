094f29234d29dbffe2d87abcbbcd85e289604c2b
Daniel Braverman <hexular@gmail.com>
2019-10-29 21:00:00 +0000

    added mocha and chai for testing. Created a couple of tests for the infoLookup helper function. Added an explanation to the helper function and its purpose

--- helpers.js
+++ helpers.js
@@ -5,4 +5,8 @@ const infoLookup = (key, value, registry) => {
   return false;
 };
 
+// the function I originally wrote returned a boolean value, which I worked off of to develop the logic for the post to /register listener
+// I found a boolean value much simpler to work with than the entire user object, as we were only checking for the existence of a value within an object
+// at no point did I have to manipulate the entire user object, so I chose not to refactor this fucntion to return the user object
+
 module.exports = { infoLookup };
---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { infoLookup } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('infoLookup', () => {
+  it('should return true for an email that exists in the registry', () => {
+    const result = infoLookup('email', "user@example.com", testUsers)
+    const expected = true;
+    assert.equal(result, expected)
+  });
+  it('should return false for an email that is not in the registry', () => {
+    const result = infoLookup('email', 'lol@ahaha.ha', testUsers)
+    const expected = false;
+    assert.equal(result, expected);
+  });
+});

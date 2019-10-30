8cba4663f53936a8b17b85dd4c180d331d3d0872
Daniel Braverman <hexular@gmail.com>
2019-10-30 14:49:31 +0000

    moved another helper function to the helpers module

--- helpers.js
+++ helpers.js
@@ -5,8 +5,17 @@ const infoLookup = (key, value, registry) => {
   return false;
 };
 
+const generateRandomString = () => {
+  let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += chars[(Math.floor(Math.random() * chars.length))];
+  }
+  return result;
+};
+
 // the function I originally wrote returned a boolean value, which I worked off of to develop the logic for the post to /register listener
 // I found a boolean value much simpler to work with than the entire user object, as we were only checking for the existence of a value within an object
 // at no point did I have to manipulate the entire user object, so I chose not to refactor this fucntion to return the user object
 
-module.exports = { infoLookup };
+module.exports = { infoLookup, generateRandomString };
